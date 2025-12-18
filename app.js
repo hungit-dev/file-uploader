43d257c6ca0537e618de472178aee01e2d5ca02c
Henry Huynh <hungitdev@gmail.com>
2025-12-18 09:07:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("./passport-config");
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+/*use passport and store sessions in memory*/
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*look and render ejs files*/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/*serve static file from specific folder*/
+app.use(express.static(assetsPath));
+
+/*read form submissions and use it values*/
+app.use(express.urlencoded({ extended: true }));
+
+/*use router*/
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}!`);
+});
