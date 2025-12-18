62d64e014f3b526e84c0bd846f66472d8bd0a38c
Hungitdev <hungitdev@gmail.com>
2025-12-18 10:09:06 -0700

    initialize prisma

---
+++ script.js
@@ -0,0 +1,22 @@
+import { prisma } from './lib/prisma.js';
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io'
+    }
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
