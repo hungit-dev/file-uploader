0ca07c59ec0d0ebf37b19a4f4100fe3d5cc523e6
Hungitdev <hungitdev@gmail.com>
2025-12-19 10:19:18 -0700

    add log in and sign up page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,8 +3,13 @@ const indexRouter = Router();
 const controller = require("../controllers/indexController");
 
 indexRouter.get("/", (req, res) => {
-  console.log("work")
   res.render("index");
 });
+indexRouter.get("/sign-up",(req,res)=>{
+  res.render("sign-up-page")
+})
+indexRouter.get("/log-in",(req,res)=>{
+  res.render("log-in-page")
+})
 
 module.exports = indexRouter;
