760ca319e0672fb32745a3bbdf2fbf8590f413c7
hungit-dev <hungitdev@gmail.com>
2026-01-13 13:41:37 -0700

    feat: use localStorage to preserve theme persistence

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
-const html=document.querySelector("html")
-const themeToggler=document.querySelector("i");
-themeToggler.addEventListener("click",()=>{
-    if(themeToggler.classList.contains("bi-sun")){
-        themeToggler.classList.remove("bi-sun");
-        themeToggler.classList.add("bi-moon");
-        html.setAttribute("data-bs-theme","dark")
-    }else{
-         themeToggler.classList.add("bi-sun");
-        themeToggler.classList.remove("bi-moon")
-        html.setAttribute("data-bs-theme","light")
-    }
-})
+const html = document.querySelector("html");
+const themeToggler = document.querySelector("i");
+
+function setTheme() {
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    themeToggler.classList.remove("bi-sun");
+    themeToggler.classList.add("bi-moon");
+  } else {
+    themeToggler.classList.remove("bi-moon");
+    themeToggler.classList.add("bi-sun");
+  }
+  html.setAttribute("data-bs-theme", theme);
+}
+
+let theme = localStorage.getItem("theme");
+if (!theme) {
+  //set light theme as default theme
+  theme = "light";
+  localStorage.setItem("theme", theme);
+}
+setTheme();
+
+themeToggler.addEventListener("click", () => {
+  theme = localStorage.getItem("theme") === "light" ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+  setTheme(theme);
+});
